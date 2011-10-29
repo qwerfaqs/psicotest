ede7cd345b9f0690770b817f6f0ccedd4d52331e
qwerfaqs <qwerfaqs@b466e5a6-cca9-30e2-6911-62de35c54f63>
2011-10-29 06:23:50 +0000

    Agregado metodo que hace if sobre una columna si es igual a otra o un valor

--- lib/test/HojaBrMujerMillon.php
+++ lib/test/HojaBrMujerMillon.php
@@ -6,7 +6,30 @@
  */
 class HojaBrMujerMillon extends BaseHojaMillon {
     var $nombre = 'Br mujer';
-    //put your code here
+    public function __construct() {
+        $criteria = new Criteria();
+        $criteria->add(HojasPeer::NOMBRE,  "BrMujer");
+        $hoja = HojasPeer::doSelectOne($criteria);
+        $valores = $hoja->getValoress();
+        foreach ($valores as $valor) {
+//            $valor = new Valores();
+            $this->hoja[$valor->getCelda()] = $valor->getValor();
+        }
+        parent::__construct();
+    }
+    public function getValor($celda) {
+        if (!isset($this->hoja[$celda]))
+            switch ($celda) {
+//            Me equivoque esto es del hombre
+//                case "C2":                 
+//                    $this->hoja[$celda] = ($this->getHoja("Resultados")->getValor("D3")) <= ($this->getHoja('BR hombre')->getValor("A2")) ? $this->getValor("B2") : 0;
+//                    break;
+
+                default:
+                    break;
+            }
+        return parent::getValor($celda);
+    }
 }
 
 ?>
