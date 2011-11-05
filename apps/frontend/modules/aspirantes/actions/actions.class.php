f0427cf0df8fcb7dfa5cf2532127f731c1fdfd51
qwerfaqs <qwerfaqs@b466e5a6-cca9-30e2-6911-62de35c54f63>
2011-11-05 18:26:41 +0000

    Importante el fixture tiene el valoresperado que faltaba, y por el cual reventaba el 16pf

--- apps/frontend/modules/aspirantes/actions/actions.class.php
+++ apps/frontend/modules/aspirantes/actions/actions.class.php
@@ -9,16 +9,16 @@
  */
 class aspirantesActions extends sfActions
 {
-  public function executeIndex(sfWebRequest $request)
-  {
-    $this->Aspirantes = AspirantesPeer::doSelect(new Criteria());
-  }
-
-  public function executeShow(sfWebRequest $request)
-  {
-    $this->Aspirante = AspirantesPeer::retrieveByPk($request->getParameter('id'));
-    $this->forward404Unless($this->Aspirante);
-  }
+//  public function executeIndex(sfWebRequest $request)
+//  {
+//    $this->Aspirantes = AspirantesPeer::doSelect(new Criteria());
+//  }
+//
+//  public function executeShow(sfWebRequest $request)
+//  {
+//    $this->Aspirante = AspirantesPeer::retrieveByPk($request->getParameter('id'));
+//    $this->forward404Unless($this->Aspirante);
+//  }
 
   public function executeNew(sfWebRequest $request)
   {
@@ -36,36 +36,36 @@ class aspirantesActions extends sfActions
     $this->setTemplate('new');
   }
 
-  public function executeEdit(sfWebRequest $request)
-  {
-    $this->forward404Unless($Aspirante = AspirantesPeer::retrieveByPk($request->getParameter('id')), sprintf('Object Aspirante does not exist (%s).', $request->getParameter('id')));
-    $this->form = new AspirantesForm($Aspirante);
-  }
-
-  public function executeUpdate(sfWebRequest $request)
-  {
-    $this->forward404Unless($request->isMethod(sfRequest::POST) || $request->isMethod(sfRequest::PUT));
-    $this->forward404Unless($Aspirante = AspirantesPeer::retrieveByPk($request->getParameter('id')), sprintf('Object Aspirante does not exist (%s).', $request->getParameter('id')));
-    $this->form = new AspirantesForm($Aspirante);
-
-    $this->processForm($request, $this->form);
-
-    $this->setTemplate('edit');
-  }
+//  public function executeEdit(sfWebRequest $request)
+//  {
+//    $this->forward404Unless($Aspirante = AspirantesPeer::retrieveByPk($request->getParameter('id')), sprintf('Object Aspirante does not exist (%s).', $request->getParameter('id')));
+//    $this->form = new AspirantesForm($Aspirante);
+//  }
+//
+//  public function executeUpdate(sfWebRequest $request)
+//  {
+//    $this->forward404Unless($request->isMethod(sfRequest::POST) || $request->isMethod(sfRequest::PUT));
+//    $this->forward404Unless($Aspirante = AspirantesPeer::retrieveByPk($request->getParameter('id')), sprintf('Object Aspirante does not exist (%s).', $request->getParameter('id')));
+//    $this->form = new AspirantesForm($Aspirante);
+//
+//    $this->processForm($request, $this->form);
+//
+//    $this->setTemplate('edit');
+//  }
   public function executeFinish(sfWebRequest $request)
   {
       
   }
 
-  public function executeDelete(sfWebRequest $request)
-  {
-    $request->checkCSRFProtection();
-
-    $this->forward404Unless($Aspirante = AspirantesPeer::retrieveByPk($request->getParameter('id')), sprintf('Object Aspirante does not exist (%s).', $request->getParameter('id')));
-    $Aspirante->delete();
-
-    $this->redirect('aspirantes/index');
-  }
+//  public function executeDelete(sfWebRequest $request)
+//  {
+//    $request->checkCSRFProtection();
+//
+//    $this->forward404Unless($Aspirante = AspirantesPeer::retrieveByPk($request->getParameter('id')), sprintf('Object Aspirante does not exist (%s).', $request->getParameter('id')));
+//    $Aspirante->delete();
+//
+//    $this->redirect('aspirantes/index');
+//  }
 
   protected function processForm(sfWebRequest $request, sfForm $form)
   {
